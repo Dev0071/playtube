d74c717f2c6ee09d9703664b2f5fa38a20b1f1e2
Dev0071 <lecturergacheru71@gmail.com>
2023-04-26 23:20:02 +0300

    implented all functionality concerning users

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,3 +1,90 @@
-export const test = (req, res) => {
-  res.json('request sucessful');
+import { creatError } from '../error.js';
+import User from '../models/User.js';
+
+// update a user
+export const updateUser = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+
+      res.status(200).json(updatedUser);
+    } catch (err) {
+      next(err);
+    }
+  } else {
+    return next(creatError(403, 'you can only update your account'));
+  }
+};
+
+//delete a user
+export const deleteUser = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+
+      res.status(200).json('user has been deleted');
+    } catch (err) {
+      next(err);
+    }
+  } else {
+    return next(creatError(403, 'you can only delete your account'));
+  }
+};
+
+// get a user
+export const getUser = async (req, res, next) => {
+  try {
+    const user = User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const like = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const dislike = async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const subscribe = async (req, res, next) => {
+  try {
+    await User.findById(req.params.id, {
+      $push: { subscribedUser: req.params.id },
+    });
+    await User.findByIdAndUpdate(req.params.id, {
+      $inc: { subscribers: 1 },
+    });
+    res.status(200).json('Subscription successful');
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const unsubscribe = async (req, res, next) => {
+  try {
+    await User.findById(req.params.id, {
+      $pull: { subscribedUser: req.params.id },
+    });
+    await User.findByIdAndUpdate(req.params.id, {
+      $inc: { subscribers: -1 },
+    });
+    res.status(200).json('Unsubscription successful');
+  } catch (error) {
+    next(error);
+  }
 };
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,36 @@
 import express from 'express';
-import {} from '../controllers/user.js';
+import {
+  deleteUser,
+  dislike,
+  getUser,
+  like,
+  subscribe,
+  unsubscribe,
+  updateUser,
+} from '../controllers/user.js';
+import { verifyToken } from '../verifyUser.js';
 
 const router = express.Router();
+
+// update a user
+router.put('/:id', verifyToken, updateUser);
+
+// delete a user
+router.delete('/:id', verifyToken, deleteUser);
+
+// get a user
+router.get('/find/:id', getUser);
+
+// subscribe a user
+router.put('/sub/:id', verifyToken, subscribe);
+
+// unsubscribe a user
+router.put('/unsub/:id', verifyToken, unsubscribe);
+
+// like a user
+router.put('/like/:videoId', verifyToken, like);
+
+// dislike a user
+router.put('/dislike/:videoId', verifyToken, dislike);
 
 export default router;
