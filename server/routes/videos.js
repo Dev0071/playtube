b24fb6a099a00af683dc3a438ab34ed06becf37f
Dev0071 <lecturergacheru71@gmail.com>
2023-05-01 22:10:03 +0300

    created routes for all actions about video

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,6 +1,33 @@
 import express from 'express';
-import {} from '../controllers/video.js';
+import {
+  addVideo,
+  deleteVideo,
+  getVideo,
+  updateVideo,
+} from '../controllers/video.js';
+import { verifyToken } from '../verifyUser.js';
 
 const router = express.Router();
+
+// create a video
+router.post('/', verifyToken, addVideo);
+
+// delete a video
+router.delete('/:id', verifyToken, deleteVideo);
+
+// update a video
+router.put('/:id', verifyToken, updateVideo);
+
+// get a video
+router.get('/find/:id', getVideo);
+
+// updateviews
+router.put('/views/:id');
+
+router.get('/trend/:id');
+
+router.get('/random/:id');
+
+router.get('/sub/:id');
 
 export default router;
